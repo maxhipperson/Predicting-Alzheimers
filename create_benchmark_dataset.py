7497c528384a94efe4bd96fe50aa95ebc15155a5
Max Hipperson <40496579+maxhipperson@users.noreply.github.com>
2020-02-25 20:59:17 +0000

    More refactoring

--- create_benchmark_dataset.py
+++ create_benchmark_dataset.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 import os
 
-def load_dataset(directory, filename):
-    csv_path = os.path.join(directory, filename)
-    df = pd.read_csv(csv_path)
-    return df
-
-def preprocessing(df):
+def main():
     
-    # --> tadpole standard preprocessing
+    # load csv
+    directory = './data'
+    
+    csv = 'TADPOLE_D1_D2'
+    new_csv = 'preprocessed_d1d2'
+    
+    csv_path = os.path.join(directory, f'{csv}.csv')
+    df = pd.read_csv(csv_path)
+    
+    ##########################
+    # Standard preprocessing #
+    ##########################
     
     # map the diagnosis values to those specified by the TADPOLE challenge
     dx_map = {
@@ -26,14 +31,10 @@ def preprocessing(df):
 
     df.loc[:, 'diagnosis'] = df['DX'].map(dx_map)
     
-    # add the age at exam
+    # add the age at exam 
     df.EXAMDATE = pd.to_datetime(df.EXAMDATE)
-    df_grouped = df.groupby('RID').apply(lambda x:(x['EXAMDATE']-x['EXAMDATE'].min()).dt.days/365.25 + x['AGE'].min())
-    df_grouped.sort_index(inplace=True)
     df.sort_values(by=['RID','EXAMDATE'], inplace=True)
-    df.loc[:, 'AGE_AT_EXAM'] = df_grouped.values
-    # df['AGE_INT'] = df['AGE_AT_EXAM'].apply(int)
-    df.reset_index(drop=True)
+    df.loc[:, 'AGE_AT_EXAM'] = df.groupby('RID').apply(lambda x:(x['EXAMDATE']-x['EXAMDATE'].min()).dt.days/365.25 + x['AGE'].min()).values
     
     # bear in mind that here we make no distrinction between catagorical and numerical fields yet
 
@@ -67,50 +68,37 @@ def preprocessing(df):
     df.sort_values(['RID', 'AGE_AT_EXAM'], inplace=True)
     for feature in features['prediction']:
         df.loc[:, f'target_{feature}'] = df.groupby('RID')[feature].shift(periods=-1).values
-        
-    # drop nans in diagnosis (this is the minimum we have to drop to add the target diagnosis)
-    df = df.dropna(subset=['diagnosis', 'target_diagnosis'])
 
     ################################
     # Making the benchmark dataset #
     ################################
     
+    # drop nans in diagnosis (this is the minimum we have to drop to add the target diagnosis)
+    benchmark_df = df.dropna(subset=['diagnosis', 'target_diagnosis'])
+    
     # select only the features we want in the benchmark dataset
     excluded_features = features['pet'] + features['csf']
-    benchmark_df = df[[feature for feature in columns_of_interest if feature not in excluded_features] + target_features]
+    benchmark_df = benchmark_df[[feature for feature in columns_of_interest if feature not in excluded_features] + target_features]
     
     # encode the diagnosis and target diagnosis
     from sklearn.preprocessing import OrdinalEncoder
     
     oe = OrdinalEncoder(categories=[['CN', 'MCI', 'AD']]) # map 'CN':0 'MCI':1 'AD':2
-    benchmark_df.loc[:, 'diagnosis_encoded'] = oe.fit_transform(benchmark_df['diagnosis'].to_numpy().reshape(-1, 1))
-    benchmark_df.loc[:, 'target_diagnosis_encoded'] = oe.transform(benchmark_df['target_diagnosis'].to_numpy().reshape(-1, 1))
+    benchmark_df.loc[:, 'diagnosis_encoded'] = oe.fit_transform(benchmark_df['diagnosis'].to_numpy().reshape(-1, 1)).astype(int)
+    benchmark_df.loc[:, 'target_diagnosis_encoded'] = oe.transform(benchmark_df['target_diagnosis'].to_numpy().reshape(-1, 1)).astype(int)
     
     # For some readson there are 2s in the APOE4 column so we'll just drop these as a quick fix
     benchmark_df = benchmark_df[benchmark_df['APOE4'] != 2.]
     
-    return df, benchmark_df
-
-def export_dataset(directory, filename, df):
-    df.to_csv(os.path.join(directory, filename), index=False)
+    ##############################
+    # Save the dataframes to csv #
+    ##############################
+    
+    new_csv_df = f'{new_csv}.csv'
+    new_csv_benchmark_df = f'{new_csv}_benchmark.csv'
+    
+    df.to_csv(os.path.join(directory, new_csv_df), index=False)
+    benchmark_df.to_csv(os.path.join(directory, new_csv_benchmark_df), index=False)
 
 if __name__ == "__main__":
-    
-    import datetime
-    now = datetime.datetime.now()
-    print(f'Version: {now.strftime("%Y-%m-%d %H:%M:%S")}')
-    
-    # load csv
-    directory = './data'
-    csv = 'TADPOLE_D1_D2.csv'
-    
-    new_csv = 'example'
-    
-    df_csv = f'{new_csv}.csv'
-    benchmark_df_csv = f'{new_csv}_benchmark.csv'
-    
-    df = load_dataset(directory, csv)
-    df, benchmark_df = preprocessing(df)
-    
-    export_dataset(directory, df_csv, df)
-    export_dataset(directory, benchmark_df_csv, benchmark_df)
+    main()
